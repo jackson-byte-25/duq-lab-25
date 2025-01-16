cb6845ab6c46199351a6c00b0b18d1e3c350160c
= <=>
2025-01-15 23:14:22 -0500

    commit all

---
+++ scripts/docstringex.py
@@ -0,0 +1,14 @@
+def add_one(x):
+    """Add one to an input variable and return the output.
+
+    Args:
+        x (_type_): the input either as a float or integer
+        
+    Returns: 
+        int: the input with one added
+        
+        
+    interesting: assert xisinstance(x, int)
+    """
+    return x + 1
+
